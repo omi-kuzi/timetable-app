5e0629d948862c4e59bffb80b57bb16ef9f5b6b5
omi-kuzi <taichi6nana8@gmail.com>
2025-05-12 10:29:50 +0900

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>時間割</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<header>
+    <h1>時間割</h1>
+</header>
+
+<div id="schedule">
+    <!-- 時間割がここに表示されます -->
+</div>
+
+<script src="js/app.js"></script>
+</body>
+</html>
